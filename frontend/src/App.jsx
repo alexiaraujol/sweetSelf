c27aa6243758943510b4bbc2f8d1d0e8242d377c
Alexia A. de Lima <alexiaalima96@gmail.com>
2026-05-08 17:01:26 -0300

    fix: corrige quantidade duplicada no carrinho

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,60 @@ import Produto from "./components/Produto";
 import { useState } from "react";
 
 function App() {
+
   const [carrinho, setCarrinho] = useState([]);
 
-  function adicionarAoCarrinho(produto){
-    setCarrinho([...carrinho,produto]);
+  function adicionarAoCarrinho(produto) {
 
+    // procura se já existe
+    const produtoExistente = carrinho.find(
+      (item) => item.id === produto.id
+    );
+
+    // se existir
+    if (produtoExistente) {
+
+      const novoCarrinho = carrinho.map((item) => {
+
+        if (item.id === produto.id) {
+
+          return {
+            ...item,
+            quantidade: item.quantidade + 1,
+          };
+        }
+
+        return item;
+      });
+
+      setCarrinho(novoCarrinho);
+
+    } else {
+
+      // se não existe adiciona produto novo
+      setCarrinho([
+        ...carrinho,
+        {
+          ...produto,
+          quantidade: 1,
+        },
+      ]);
+    }
   }
-  const total = carrinho.reduce((acc, item) => acc + item.preco, 0);
+
+  function removerDoCarrinho(id) {
+
+    const novoCarrinho = carrinho.filter(
+      (item) => item.id !== id
+    );
+
+    setCarrinho(novoCarrinho);
+  }
+
+  const total = carrinho.reduce(
+    (acc, item) => acc + item.preco * item.quantidade,
+    0
+  );
 
   return (
     <div style={{ padding: "20px" }}>
@@ -26,17 +73,33 @@ function App() {
       ))}
 
       <h2>Carrinho</h2>
-      
+
       {carrinho.length === 0 ? (
-        <p>Seu carrinho esta vazio</p>
-      ): (
-        carrinho.map((item,index) => (
-          <p key={index}>
-            {item.nome} - R${item.preco.toFixed(2)}
-          </p>
+
+        <p>Seu carrinho está vazio</p>
+
+      ) : (
+
+        carrinho.map((item) => (
+
+          <div key={item.id}>
+
+            <p>
+              {item.nome} -
+              Quantidade: {item.quantidade} -
+              R$ {(item.preco * item.quantidade).toFixed(2)}
+            </p>
+
+            <button onClick={() => removerDoCarrinho(item.id)}>
+              Remover item
+            </button>
+
+          </div>
         ))
       )}
+
       <h3>Total: R$ {total.toFixed(2)}</h3>
+
     </div>
   );
 }
--- frontend/src/data/produtos.js
+++ frontend/src/data/produtos.js
@@ -7,17 +7,19 @@ export const produtos = [
       "https://www.oetker.com.br/assets/recipes/assets/a81bc035eb7f407faaa2c93e04edaf78/750x910/bolo-de-aniversrio-de-chocolate.webp",
   },
   {
-    id: 1,
+    id: 2,
     nome: "Bolo Três Amores",
     preco: 18.0,
     imagem:
       "https://www.oetker.com.br/assets/recipes/assets/a81bc035eb7f407faaa2c93e04edaf78/750x910/bolo-de-aniversrio-de-chocolate.webp",
   },
   {
-    id: 1,
+    id: 3,
     nome: "Bolo Prestigio",
     preco: 18.0,
     imagem:
       "https://www.oetker.com.br/assets/recipes/assets/a81bc035eb7f407faaa2c93e04edaf78/750x910/bolo-de-aniversrio-de-chocolate.webp",
   },
 ];
+
+
